2472f8de4f436560efd3ed2048d9d170aabbb8e2
kimjoongmin <82187845+kimjoongmin@users.noreply.github.com>
2021-04-09 18:02:36 +0900

    1

--- src/App.vue
+++ src/App.vue
@@ -95,7 +95,7 @@ export default {
       modalWrap : false,
       product: ['강남구','강북구','마포구'],
       price: [100,200,300],
-      menuName: ['Home','About','Room','Board'],
+      menuName: ['Home','About','Room','Board1'],
       Num: [0,0,0],
       txtRed: 'color:red',
       hands: 'cursor:pointer',
